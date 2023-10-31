010222c6a94f2b6ebd916a7a0486a926ad621ec5
Yash0125 <116873935+Yash0125@users.noreply.github.com>
2023-10-31 15:51:01 +0530

    chore : react-icons added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
+import Sidebar from './components/Sidebar';
 
 function App() {
   return (
-    <div className="bg">
-      
-    </div>
+    <>
+    <Sidebar/> 
+    </>
   );
 }
 
